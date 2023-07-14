36a0af2f1e2d17c7553de970b041c0b4b37f2682
Justin Wright <djw6497@gmail.com>
2023-07-13 19:16:55 -0500

    Takeoff Button now launches the rocket, makes background blue, and increases shuttle height to 10000 miles.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,20 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+function init() {
+    const takeOffButton = document.getElementById('takeoff');
+
+    const flightStatus = document.getElementById('flightStatus');
+    const shuttleBackground = document.getElementById('shuttleBackground');
+    const shuttleHeight = document.getElementById('spaceShuttleHeight');
+
+    takeOffButton.addEventListener('click', function (event) {
+        let takeoffReady = window.confirm('Confirm that the shuttle is ready for takeoff.');
+        if (takeoffReady === true) {
+            flightStatus.innerHTML = 'Shuttle in flight.';
+            shuttleBackground.style.background = 'blue';
+            shuttleHeight.innerHTML = 10000;
+        }
+    });
+};
+
+window.addEventListener('load',init);
